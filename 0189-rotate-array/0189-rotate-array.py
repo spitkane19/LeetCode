da3d39182a826293630f6ff6fd923bb24631f3e4
spitkane19 <55877672+spitkane19@users.noreply.github.com>
2024-03-09 12:55:25 +0200

    Time: 142 ms (92.20%), Space: 23.1 MB (90.48%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        
+        lngth = len(nums)
+        fake_list = list(nums)
+        real_k = k % lngth       
+        for i, x in enumerate(nums):
+            move = i + real_k - lngth
+            fake_list[move] = x
+            
+        nums[:] = fake_list
+            
